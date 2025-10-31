1a458c4faf80bfd0bf12e3ff9ddc19d095f95c9b
andremrahman <ndrehehe@gmail.com>
2025-10-31 18:06:56 +0700

    home dan layanan

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,77 @@
 <x-layout>
   <x-slot:title>{{ $title }}</x-slot:title>
-  <p>Kami berkomitmen menghadirkan hasil konstruksi terbaik dengan mengutamakan ketepatan waktu, mutu, dan kepuasan
-    klien.</p>
+  <section id="hero">
+    <p>Kami berkomitmen menghadirkan hasil konstruksi terbaik dengan mengutamakan ketepatan waktu, mutu, dan kepuasan
+      klien.</p>
+    <a class="group inline-block rounded-xl bg-linear-to-r from-pink-500 via-red-500 to-yellow-500 p-0.5 hover:text-white focus:ring-3 focus:outline-hidden"
+      href="#">
+      <span class="block rounded-lg bg-white px-8 py-3 text-sm font-medium group-hover:bg-transparent">
+        Lihat Layanan Kami
+      </span>
+    </a>
+
+  </section>
+  <section id="profil">
+    <h2>Profil Perusahaan</h2>
+    <p class="mb-5">Berbekal keahlian dan pengalaman dalam menangani proyek-proyek prestisius berskala menengah dan
+      besar. <strong>PT Gajahmada Karya Mandiri (GKM)</strong> berpengalaman dalam perancanaan dan pembangunan pabrik,
+      apartemen, gedung
+      komersial, hotel, rumah sakit, infrastruktur (jembatan, pembukaan kawasan industri, jalan tol, jalan raya,
+      flyover) serta pekerjaan pemancangan tiang pancang yang dikerjakan tersebar di berbagai daerah di Indonesia.
+    <p class="mb-5">
+      PT Gajahmada Karya Mandiri (GKM) selalu berupaya memberikan hasil yang sesuai dengan kontrak yang disepakati.
+      Dengan pengalaman dan keterampilan yang teruji, kami bangga untuk memberikan solusi dan layanan terbaik untuk
+      menggapai tujuan proyek anda.
+      Kualitas sumber daya manusia dan profesionalitas menjadi kunci kehandalan layanan kami. Kami mengandalkan tenaga
+      profesional yang mahir dibidangnya untuk senantiasa memberikan solusi terbaik dalam kebutuhan konstruksi.
+
+    <p class="mb-5">
+      PT Gajahmada Karya Mandiri (GKM) bergerak di jasa kontraktor umum dengan pelayanan yang lengkap dan menyeluruh
+      meliputi : perencanaan, desain, hingga pembangunan (plan, design and build).
+
+    </p>
+    </p>
+    <p class="mb-5">
+
+      Dalam penanganan proyek-proyeknya, kami juga menawarkan kemungkinan pelaksanaan pekerjaan melalui kerjasama
+      operasi (joint operation).
+      Melaksanakan jasa konstruksi secara joint operation. Dalam skema ini kami membuka kemungkinan untuk bekerja sama
+      dengan perusahaan konstruksi lain, baik lokal ataupun asing untuk mengerjakan proyek secara bersama-sama.
+      Memberikan jasa konstruksi secara menyeluruh yang meliputi perencanaan awal, desain, detail penanganan hingga
+      konstruksi dengan jaminan harga dan waktu penyelesaian yang efektif dan efisien.
+    </p>
+
+    </p>
+  </section>
+  <section id="visimisi">
+    <h2>Visi</h2>
+    <p>Menjadi Perusahaan Jasa Konstruksi yang terkemuka dan terpecaya dengan menyediakan produk-produk berkualitas dan
+      menciptakan nilai yang optimal bagi pelanggan, pemegang saham, karyawan dan masyarakat Mendukung kemajuan
+      pembangunan bangsa Indonesia melalui pembangunan proyek berskala besar maupun kecil, untuk memberikan kepuasan
+      pelanggan melalui ketepatan dalam segi kualitas, waktu penyelesaian pekerjaan dan biaya dengan didukung oleh
+      kehandalan Sumber Daya Manusia dan menggunakan teknologi yang paling efisien
+    </p>
+    <h2>Misi</h2>
+    <ul>
+      <li>Memastikan kepuasan klien</li>
+      <li>Usaha perbaikan pelayanan secara berkesinambungan</li>
+      <li>Lugas dan jelas dalam memberi jasa konsultasi</li>
+      <li>Tepat waktu dalam pengerjaan</li>
+      <li>Informatif</li>
+      <li>Patuh terhadap persyaratan klien dan peraturan perundang-undangan</li>
+      <li>Lihai dalam melihat peluang perbaikan</li>
+      <li>Edukasi terencana untuk peningkatan kompetensi karyawan</li>
+    </ul>
+  </section>
+  <section id="sdm">
+    <h2>Sumber Daya Manusia</h2>
+    <p>Tim kami mempunyai komitmen tinggi untuk memenuhi kebutuhan proyek dengan keahlian yang mereka miliki. Kami
+      mempunyai sumber daya manusia dari berbagai disiplin ilmu untuk memaksimalkan kualitas yang akan kami sampaikan ke
+      klien.
+    </p>
+    <p>GKM dapat memenuhi harapan mitra sekarang dan nanti. GKM selalu berusaha menjaga kualitas sumber daya manusia.
+      Dengan menciptakan lingkungan perusahaan yang membuat tim kami terus belajar dan berkembang. Menyelesaikan
+      pekerjaan dengan cepat dan solutif menjadi komitmen bersama.
+    </p>
+  </section>
 </x-layout>
--- resources/views/layanan.blade.php
+++ resources/views/layanan.blade.php
@@ -1,4 +1,19 @@
 <x-layout>
-    <x-slot:title>{{ $title }}</x-slot:title>
-    <h3 class="text-xl">layanan kami</h3>
+  <x-slot:title>{{ $title }}</x-slot:title>
+  <h1 class="text-xl">Layanan Kami</h1>
+  <p>
+    Kami menyediakan layanan profesional untuk kebutuhan proyek konstruksi anda, antara lain:
+  </p>
+  <ul>
+    <li>Tata ruang dan wilayah</li>
+    <li>Arsitektur Sipil</li>
+    <li>Struktur Jalan</li>
+    <li>Jembatan</li>
+    <li>Geoteknik</li>
+    <li>Mekanikal</li>
+    <li>Elektrikal</li>
+    <li>Plumbing</li>
+    <li>Bidang layanan lainnya</li>
+  </ul>
+
 </x-layout>
