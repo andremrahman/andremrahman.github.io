c8eccd79da5f66cd8e644dec8056e3051ac5ed4e
andremrahman <ndrehehe@gmail.com>
2025-10-31 14:56:13 +0700

    mulai home

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en" class="h-full bg-gray-100 dark:bg-gray-900">
+<html lang="en" class="h-full bg-gray-100">
 
 <head>
   <meta charset="UTF-8">
@@ -17,13 +17,6 @@
 
   <div class="min-h-full">
     <x-navbar></x-navbar>
-
-    {{-- <header
-            class="relative bg-gray-800 after:pointer-events-none after:absolute after:inset-x-0 after:inset-y-0 after:border-y after:border-white/10">
-            <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-                <h1 class="text-3xl font-bold tracking-tight text-white">Dashboard</h1>
-            </div>
-        </header> --}}
     <main>
       <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
         <!-- Your content -->
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,5 @@
 <x-layout>
-    <x-slot:title>{{ $title }}</x-slot:title>
-    <h3 class="text-xl">Home</h3>
+  <x-slot:title>{{ $title }}</x-slot:title>
+  <p>Kami berkomitmen menghadirkan hasil konstruksi terbaik dengan mengutamakan ketepatan waktu, mutu, dan kepuasan
+    klien.</p>
 </x-layout>
