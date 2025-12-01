92cb51f4b86c74ec73dc07e80d28228de0d2c81d
andremrahman <ndrehehe@gmail.com>
2025-12-01 20:42:28 +0700

    preload fix 2

--- resources/views/components/preload.blade.php
+++ resources/views/components/preload.blade.php
@@ -1,19 +1,19 @@
-<!-- 1. PRELOADER WRAPPER -->
+<!-- PRELOADER WRAPPER -->
 <div id="preloader-wrapper" class="preloader-wrapper">
 
-  <!-- Konten Teks dan Spinner di Tengah -->
+  <!-- Konten -->
   <div id="preloader-content" class="preloader-content">
-    <img id="logo-image" src="https://placehold.co/150x50/FBBF24/111827?text=PT+Logo" alt="PT abcdefghi Logo"
+    <img id="logo-image" src="{{ asset('img/logo.png') }}" alt="Logo" class="w-40 sm:w-48 md:w-56 lg:w-64"
       onerror="this.style.display='none'; document.getElementById('logo-text-fallback').style.display='block';">
     <h3 id="logo-text-fallback" class="text-4xl sm:text-5xl font-extrabold tracking-wider text-gray-800"
       style="display:none;">
-      PT abcdefghi
+      PT GAJAHMADA KARYA MANDIRI
     </h3>
 
     <div class="spinner"></div>
   </div>
 
-  <!-- 6 Kolom yang Akan Meluncur (Tirai) -->
+  <!-- (Tirai) -->
   <div class="hero__inner constrain">
     <div class="hero__image-cont">
       <div class="anim-swipe"></div>
